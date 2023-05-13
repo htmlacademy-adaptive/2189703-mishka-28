060112c7f9f007a3e4c80d19b3555ab8b813b454
firefoxic <firefoxic.dev@gmail.com>
2023-05-13 06:15:37 +0300

    Update automation

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,11 @@
-let headerNav = document.querySelector('.header-nav');
-let headerToggle = document.querySelector('.header-nav__toogle');
+let headerNav = document.querySelector(`.header-nav`)
+let headerToggle = document.querySelector(`.header-nav__toogle`)
 
-headerNav.classList.remove('header-nav--nojs');
+headerNav.classList.remove(`header-nav--nojs`)
+headerNav.classList.add(`header-nav--closed`)
+headerNav.classList.remove(`header-nav--opened`)
 
-headerNav.addEventListener('click', function() {
-	if (headerNav.classList.contains('header-nav--closed')) {
-		headerNav.classList.remove('header-nav--closed');
-		headerNav.classList.add('header-nav--opened');
-	} else {
-		headerNav.classList.add('header-nav--closed');
-		headerNav.classList.remove('header-nav--opened');
-	}
+headerToggle.addEventListener(`click`, () => {
+	headerNav.classList.toggle(`header-nav--closed`)
+	headerNav.classList.toggle(`header-nav--opened`)
 })
